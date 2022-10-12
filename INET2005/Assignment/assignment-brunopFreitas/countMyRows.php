9b14f2d1540e570aa8b1979e5dd0390080222d20
brunopfreitas <w0448225@nscc.ca>
2022-10-12 12:26:41 -0300

    assignment inet2005

---
+++ INET2005/Assignment/assignment-brunopFreitas/countMyRows.php
@@ -0,0 +1,13 @@
+<?php
+function countMyRows($myReq) {
+    if ($myQuerie == 'req1') {
+
+    } elseif ($my)
+    //Results quantity
+    $total_rows = mysqli_num_rows(countEmployee());
+//Total number of rows;
+    $total_pages = ceil($total_rows/$maxRows);
+}
+
+
+?>
--- INET2005/Assignment/assignment-brunopFreitas/main.php
+++ INET2005/Assignment/assignment-brunopFreitas/main.php
@@ -1,7 +1,7 @@
 <?php
+session_start();
 $fileList = array(
     'req1.php',
-    'req2.php',
     'req3.php'
 );
 
@@ -10,9 +10,26 @@ foreach($fileList as $fileName)
 {
     include_once($dirPath.$fileName);
 }
-
+//Global Variables
 $maxRows = 25;
-$page = 0;
+//Defining the page value
+if(!isset($_POST['action'])) {
+    $page = 0;
+} elseif ($_POST['action'] == 'Next') {
+    //Next button value
+    $page = intval($_POST['page'])+1;
+} elseif ($_POST['action'] == 'Previous') {
+    //Previous button value
+    $page = intval($_POST['page'])-1;
+}
+//
+if ($_POST['word'] && !$_POST['Clear']) {
+    $_SESSION["condition"] = $_POST['word'];
+} elseif ($_POST['Clear']) {
+    $_SESSION["condition"] = '';
+    $page = 0;
+}
+    $condition = $_SESSION["condition"];
 
 ?>
 
@@ -24,13 +41,16 @@ $page = 0;
 <body>
 <style>
 th, tr, td { border: solid 1px black;}
+#emp-nmb, textarea {
+    background-color: gray;
+}
 </style>
 <h1>INET2005 - Bruno Freitas - Assignment 1</h1>
 <p>REQ 1, 2, 3</p>
 <form action="main.php" method="post">
 <!--    Sticky Form-->
-    <p>Search: <input type="text" name="word" value="<?php if (isset($_POST ['word'])) echo $_POST['word'];?>"></p>
-    <p><input type="submit" name="Submit" value="Submit"></p>
+    <p>Search: <input type="text" name="word" value="<?php echo $condition?>"></p>
+    <p><input type="submit" name="Submit" value="Search"><input type="submit" name="Clear" value="Clear"></p>
 </form>
 <table>
 <thead>
@@ -40,63 +60,47 @@ th, tr, td { border: solid 1px black;}
 <th>Last Name</th>
 <th>Gender</th>
 <th>Hire Date</th>
+<th>Add/Edit</th>
+<th>Delete</th>
 </thead>
 <tbody>
+<form action="main.php" method="post">
+    <p><input type="submit" name="addBtn" value="Add"></p>
+</form>
 <?php
-//Quantity results
-$total_rows = mysqli_num_rows(countEmployee());
+if($_POST['addBtn']) {
+    echo "<tr>";
+    echo "<td><input type='text' id='emp-nmb' name='emp-nmb' disabled></td>";
+    echo "<td><input type='text' id='emp-brt-dt' name='emp-brt-dt'></td>";
+    echo "<td><input type='text' id='emp-fn' name='emp-fn'></td>";
+    echo "<td><input type='text' id='emp-ln' name='emp-ln'></td>";
+    echo "<td><input type='text' id='emp-g' name='emp-g'></td>";
+    echo "<td><input type='text' id='emp-hd' name='emp-hd'></td>";
+    echo "<td><input type='submit' name='add' value='Add/Edit'></td>";
+    echo "<td><input type='submit' name='delete' value='Delete'></td>";
+    echo "</tr>";
+}
+?>
 
-//echo $total_rows;
-$total_pages = ceil($total_rows/$maxRows);
-
-//Display first time
-if(!isset($_POST["word"]) && !isset($_POST['action'])) {
-    $result = displayEmployeePage(0, $maxRows);
-    while($row = mysqli_fetch_assoc($result)):
-    ?>
-    <tr>
-        <td><?php echo $row['emp_no'] ?></td>
-        <td><?php echo $row['birth_date'] ?></td>
-        <td><?php echo $row['first_name'] ?></td>
-        <td><?php echo $row['last_name'] ?></td>
-        <td><?php echo $row['gender'] ?></td>
-        <td><?php echo $row['hire_date'] ?></td>
-    </tr>
-
-<?php endwhile ?>
 <?php
-//    Next Button
-} elseif (!isset($_POST["word"]) && isset($_POST['action'])) {
-    if ($_POST['action'] == 'Next') {
-        $page = intval($_POST['page'])+1;
-        if ($page > $total_pages) {
-            $page = $total_pages;
-        }
-        $start = $page*$maxRows;
-        if ($start > $total_rows) {
-            $start = $total_rows -1;
-        }
-        $result = displayEmployeePage($start, $maxRows);
-        while($row = mysqli_fetch_assoc($result)):
-            ?>
-            <tr>
-                <td><?php echo $row['emp_no'] ?></td>
-                <td><?php echo $row['birth_date'] ?></td>
-                <td><?php echo $row['first_name'] ?></td>
-                <td><?php echo $row['last_name'] ?></td>
-                <td><?php echo $row['gender'] ?></td>
-                <td><?php echo $row['hire_date'] ?></td>
-            </tr>
-
-        <?php endwhile ?>
- <?php
-        //    Previous Button
-    } elseif ($_POST['action'] == 'Previous') {
-        $page = intval($_POST['page'])-1;
+// Displaying employee info
+if (!$condition) {
+    //    Local variables
+    //    Result quantity
+    $total_rows = mysqli_num_rows(countEmployee());
+    if ($total_rows > 0) {
+        //      Page quantity
+        $total_pages = ceil($total_rows/$maxRows);
         if ($page < 0) {
             $page = 0;
         }
+        if ($page+1 >= $total_pages) {
+            $page = $total_pages-1;
+        }
         $start = $page*$maxRows;
+        if ($start > $total_rows) {
+            $start = $total_rows-1;
+        }
         $result = displayEmployeePage($start, $maxRows);
         while($row = mysqli_fetch_assoc($result)):
             ?>
@@ -107,38 +111,61 @@ if(!isset($_POST["word"]) && !isset($_POST['action'])) {
                 <td><?php echo $row['last_name'] ?></td>
                 <td><?php echo $row['gender'] ?></td>
                 <td><?php echo $row['hire_date'] ?></td>
+                <td><input type="submit" name="add" value="Add/Edit"></td>
+                <td><input type="submit" name="delete" value="Delete"></td>
             </tr>
 
-        <?php endwhile ?>
-<?php
-//    No button
+        <?php endwhile;
     } else {
-        "<p>Wrong action!</p>";
+        $total_pages = 1;
+        echo "<p>Nothing to display</p>";
     }
-}
+        ?>
 
+<?php
+//Search my result
+} elseif($condition || isset($_POST['Submit'])) {
 
-//WHERE my result
-if(isset($_POST["word"])) {
-    $condition = $_POST['word'];
-    $result = findEmployee($condition,0,25);
-    $row = mysqli_fetch_assoc($result);
-    if($row!=0) {
-        while($row = mysqli_fetch_assoc($result)):
-            ?>
-            <tr>
-                <td><?php echo $row['emp_no'] ?></td>
-                <td><?php echo $row['birth_date'] ?></td>
-                <td><?php echo $row['first_name'] ?></td>
-                <td><?php echo $row['last_name'] ?></td>
-                <td><?php echo $row['gender'] ?></td>
-                <td><?php echo $row['hire_date'] ?></td>
-            </tr>
+    //    Local variables
+    //    Result quantity
+    $total_rows = mysqli_num_rows(findEmployee($condition));
+    if ($total_rows>0) {
+        //    Page quantity
+        $total_pages = ceil($total_rows/$maxRows);
+        if ($page < 0) {
+            $page = 0;
+        }
+        if ($page+1 >= $total_pages) {
+            $page = $total_pages-1;
+        }
+        $start = $page*$maxRows;
+        if ($start > $total_rows) {
+            $start = $total_rows;
+        }
+        $result = findEmployeeLimit($condition,$start, $maxRows);
+        $row = mysqli_fetch_assoc($result);
+        if($row!=0) {
+            while($row = mysqli_fetch_assoc($result)):
+                ?>
+                <tr>
+                    <td><?php echo $row['emp_no'] ?></td>
+                    <td><?php echo $row['birth_date'] ?></td>
+                    <td><?php echo $row['first_name'] ?></td>
+                    <td><?php echo $row['last_name'] ?></td>
+                    <td><?php echo $row['gender'] ?></td>
+                    <td><?php echo $row['hire_date'] ?></td>
+                    <td><input type="submit" name="add" value="Add/Edit"></td>
+                    <td><input type="submit" name="delete" value="Delete"></td>
+                </tr>
 
-        <?php
-        endwhile;
+            <?php
+            endwhile;
+        }else {
+            echo "<p>Nothing to display.</p>";
+        }
     } else {
-        echo "<p>Nothing to display.</p>";
+        $total_pages = 1;
+        echo "<p>No results for $condition</p>";
     }
 }
 ?>
@@ -146,7 +173,7 @@ if(isset($_POST["word"])) {
 </table>
 <form action="main.php" method="post">
     <p><?php echo 'Total Results: ', $total_rows ?></p>
-    <p><?php $pageResult = $page; echo 'Row number: ', ++$pageResult ?></p>
+    <p><?php $pageResult = $page; echo 'Page: ', ++$pageResult, ' of ', $total_pages ?></p>
     <input type="hidden" name="page" value="<?php echo $page; ?>">
     <input type="submit" name="action" value="Previous" />
     <input type="submit" name="action" value="Next" />
