911c13a836c0a746adaa42ef7a80e5b9a7351a04
brunopfreitas <w0448225@nscc.ca>
2022-10-19 10:16:35 -0300

    INET2005 LAB5

--- INET2005/Assignment/assignment-brunopFreitas/dbconn.php
+++ INET2005/Assignment/assignment-brunopFreitas/dbconn.php
@@ -1,7 +1,16 @@
 <?php
     function getDbConnection()
     {
-        $conn = mysqli_connect("localhost", "root", "inet2005", "sakila");
+        // IF in a modern way
+        // $host = isset($_ENV["DB_HOST"]) ? $_ENV["DB_HOST"] : "localhost
+        // $host = $_ENV["DB_HOST"] ?? "localhost"
+
+        $host = $_ENV["DB_HOST"] ?? "localhost";
+        $username = $_ENV["DB_USERNAME"] ?? "root";
+        $password = $_ENV["DB_PASSWORD"] ?? "inet2005";
+        $database = $_ENV["DB_DATABASE"] ?? "employees";
+
+        $conn = mysqli_connect($host, $username, $password, $database);
         if(!$conn)
         {
             die("Unable to connect to database: " . mysqli_connect_error());
