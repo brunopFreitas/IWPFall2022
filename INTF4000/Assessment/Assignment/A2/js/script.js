52ab3906b046671643ad78090666d1dc84e0e9f6
Freitas <W0448225@campus.nscc.ca>
2022-10-20 11:55:41 -0300

    INFT4000 A2

--- INTF4000/Assessment/Assignment/A2/js/script.js
+++ INTF4000/Assessment/Assignment/A2/js/script.js
@@ -1,32 +1,40 @@
 const musicTable = document.getElementById('musicTable')
 
+const extAccepted = {
+    songExt: ['.mp3', '.wav'],
+    imgExt: ['.jpg', '.png']
+}
+
 window.electronAPI.onSendToFront((_event, albumObject) => {
     let tbody = document.createElement('tbody')
 
     albumObject.forEach((songObject) => {
-        if (songObject.hasOwnProperty('songName')) {
-            let row = document.createElement('tr')
-            let cell = document.createElement('td')
-            cell.innerHTML = songObject.songName
-            cell.setAttribute('url',songObject.songURL)
-            cell.setAttribute("id", "track")
-            row.appendChild(cell);
-            tbody.appendChild(row);
-            musicTable.appendChild(tbody)
-        }
-    })
-
-    const coverArt = document.getElementById('cover')
-    albumObject.forEach((songObject) => {
-        if(songObject.coverName=="Large" && songObject.hasOwnProperty('coverURL')) {
-            coverArt.src = songObject.coverURL
-        }
+        extAccepted.songExt.forEach((ext) => {
+            if (songObject.ext === ext) {
+                let row = document.createElement('tr')
+                let cell = document.createElement('td')
+                cell.innerHTML = songObject.name
+                cell.setAttribute('url', songObject.url)
+                cell.setAttribute("id", "track")
+                row.appendChild(cell)
+                tbody.appendChild(row)
+                musicTable.appendChild(tbody)
+            } else {
+                extAccepted.imgExt.forEach((ext) => {
+                    if (songObject.ext === ext) {
+                        const coverArt = document.getElementById('cover')
+                        coverArt.src = songObject.url
+                    }
+                })
+            }
+        })
     })
 })
 
-$(document).on('click','#track',function(){
+$(document).on('click', '#track', function () {
     let url = $(this).attr('url')
     const audio = document.getElementById('audio')
+    const songName = document.getElementById('songName')
     audio.src = url
-
+    songName.innerHTML = $(this).text()
 })
--- INTF4000/Assessment/Assignment/A2/main.js
+++ INTF4000/Assessment/Assignment/A2/main.js
@@ -4,7 +4,7 @@ const { app, BrowserWindow, ipcMain } = require('electron')
 
 const createWindow = () => {
     const win = new BrowserWindow({
-        width: 800,
+        width: 500,
         height: 600,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js')
@@ -18,6 +18,7 @@ const createWindow = () => {
 
 app.whenReady().then(() => {
     const mainWindow = createWindow()
+    let albumObject = getMyMusic()
 
     mainWindow.webContents.once('dom-ready', () => {
         mainWindow.webContents.send('sendToFront', albumObject)
@@ -36,55 +37,35 @@ app.on('window-all-closed', () => {
 
 // **************** BACKEND PROCESSESS ************************
 
-const fs = require('fs')
-const url = require('url')
+function getMyMusic() {
+  const fs = require('fs')
+  const url = require('url')
 
-// Path to mp3 files
+  // Path to mp3 files
+  // Path to the song folder 
+  const directoryPath = "C:\\Users\\w0448225\\Documents\\brunoW0448225\\INTF4000\\Albuns\\The Smiths\\Hatful Of Hollow"
 
-// Path to the song folder 
-const songDirectoryPath = "C:\\Users\\w0448225\\Documents\\brunoW0448225\\INTF4000\\Albuns\\The Smiths\\Hatful Of Hollow\\song"
+  // albumObject
+  let albumObject = []
 
-// Path to the img folder
-const imgDirectoryPath = "C:\\Users\\w0448225\\Documents\\brunoW0448225\\INTF4000\\Albuns\\The Smiths\\Hatful Of Hollow\\img"
+  // Reading Songs
+  let album = fs.readdirSync(directoryPath, function (err, files) {
+    //handling error
+    if (err) {
+      return console.log('Unable to scan directory: ' + err)
+    }
+  })
 
-// albumObject
+  //listing all files using forEach
+  album.forEach(function (albumElement) {
+    let songObject = {}
+    songObject["name"] = path.parse(albumElement).name
+    songObject["ext"] = path.parse(albumElement).ext
+    songObject["url"] = url.pathToFileURL(path.resolve(directoryPath, albumElement)).href
+    songObject["path"] = path.resolve(albumElement)
+    albumObject.push(songObject)
+  })
 
-let albumObject = []
-
-// Reading Songs
-let songs = fs.readdirSync(songDirectoryPath, function (err, files) {
-  //handling error
-  if (err) {
-    return console.log('Unable to scan directory: ' + err);
-  }
-})
-
-//listing all files using forEach
-songs.forEach(function (song) {
-  let songObject = {
-
-  }
-  songObject["songName"] = path.parse(song).name
-  songObject["songURL"] = url.pathToFileURL(path.resolve(songDirectoryPath, song)).href
-  songObject["songPath"] = path.resolve(song)
-  albumObject.push(songObject)
-});
-
-// Reading cover
-let covers = fs.readdirSync(imgDirectoryPath, function (err, files) {
-  //handling error
-  if (err) {
-    return console.log('Unable to scan directory: ' + err);
-  }
-})
-
-//listing all files using forEach
-covers.forEach(function (cover) {
-  let songObject = {
-
-  }
-  songObject["coverName"] = path.parse(cover).name
-  songObject["coverURL"] = url.pathToFileURL(path.resolve(imgDirectoryPath, cover)).href
-  songObject["coverPath"] = path.resolve(cover)
-  albumObject.push(songObject)
-});
+  console.log(albumObject)
+  return albumObject
+}
