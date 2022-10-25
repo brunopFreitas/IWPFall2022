1c9bc3022b68407f32dab5df348888e191d2c253
brunopFreitas <w0448225@nscc.ca>
2022-10-25 09:14:18 -0300

    INET2005 captcha

--- INET2005/Assignment/lab5-brunopFreitas/register.php
+++ INET2005/Assignment/lab5-brunopFreitas/register.php
@@ -1,13 +1,9 @@
 <html lang="en">
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="main.js"></script>
+    <link rel="stylesheet" href="style.css">
     <title>INET2005 - Lab 5</title>
-    <style>
-        form {
-            border: 1px solid black;
-        }
-    </style>
+    <script src="main.js" defer></script>
 </head>
 <body>
 <h1>New User Register Form</h1>
@@ -32,14 +28,30 @@ if(isset($_POST['Submit'])) {
             }
         }
     } else {
-        echo "<h2>Password should be at least 8 characters in length and should include at least one upper case letter, one number, and one special character.</h2> ";
+        echo "<h2 id='passwordText'>Password should be at least 8 characters in length and should include at least one upper case letter, one number, and one special character.</h2> ";
     }
 }
 ?>
-<form action="register.php" method="post" id="form" <?php if ($showFormFlag==false){?>style="display:none"<?php } ?>>
+<form action="register.php" method="post" id="loginForm" <?php if ($showFormFlag==false){?>style="display:none"<?php } ?> onsubmit="return checkform(this);">
     <p>Username: <input type="text" name="username" id="userName" status="false" value="<?php echo $_POST['username']?>"></p>
     <p>Password: <input type="password" name="password" id="password" status="false"></p>
     <p><input type="button" name="Submit" id="Submit" value="Insert" onclick="validateRegisterForm()"></p>
+    <!-- START CAPTCHA -->
+    <br>
+    <div class="capbox">
+
+        <div id="CaptchaDiv"></div>
+
+        <div class="capbox-inner">
+            Type the number:<br>
+
+            <input type="hidden" id="txtCaptcha">
+            <input type="text" name="CaptchaInput" id="CaptchaInput" size="15"><br>
+
+        </div>
+    </div>
+    <br><br>
+    <!-- END CAPTCHA -->
 </form>
 </body>
 </html>
